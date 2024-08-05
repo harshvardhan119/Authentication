a859ea938b69312c752122c3c54c65e043dbfd28
harshvardhan119 <131952048+harshvardhan119@users.noreply.github.com>
2024-08-05 23:48:34 +0530

    Delete models directory

--- models/Car.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const carSchema = new mongoose.Schema({
-  carName: { type: String, required: true },
-  manufacturingYear: { type: Number, required: true },
-  price: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('Car', carSchema);
